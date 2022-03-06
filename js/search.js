313bf87d3eeddcd5ed6434d16295c7e63eb0657d
Devesh Jain <deveshsolapur@gmail.com>
2022-03-06 14:59:54 +0530

    update

--- js/search.js
+++ js/search.js
@@ -1,56 +1,69 @@
 $(document).ready(function(){
     $("#filter").keyup(function(){
-
-
-        var filter = $(this).val(), count=0;
-
-
+ 
+        // Retrieve the input field text and reset the count to zero
+        var filter = $(this).val(), count = 0;
+ 
+        // Loop through the comment list
         $("nav ul li.search").each(function(){
-
-
-            if ($(this).text().search(new RegExp (filter, "i")) <0){
+ 
+            // If the list item does not contain the text phrase fade it out
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
                 $(this).fadeOut();
-
-                
+ 
+            // Show the list item if the phrase matches and increase the count by 1
             } else {
                 $(this).show();
                 count++;
             }
-    });
-
-
-    let result;
-    $("#filter-count").text("Number of results = "+count);
-    if (count ==0){
-        document.getElementById("cond").innerHTML = `No disease found`;
-
-    } else{ `<p></p>`}
-}
-);
-
+        });
+ 
+        // Update the count
+        
+        let result;
+        $("#filter-count").text("Number of Results = "+count);
+        if (count == 0){
+          document.getElementById("cond").innerHTML = `No Results Found`;
+        }
+        else{ `<p><p/>`
+        }
+        }
+    );
 });
 
-// modal 
+// Modal
 
 var btn = document.querySelectorAll("button.modalbutton");
 
-var modals = document.querySelectorAll(".modal");
+// All page modals
+var modals = document.querySelectorAll('.modal');
 
+// Get the <span> element that closes the modal
 var spans = document.getElementsByClassName("close");
- 
-for (var i = 0; i < spans.length; i++){
-    spans[i].onclick = function(){
-        for(var index in modals){
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-        }
-    }
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
 }
 
-window.onclick = function(event){
-    if (event.target.classList.contains('modal')
-    ){
-        for(var index in modals){
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
     }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
     }
-    }
+}
